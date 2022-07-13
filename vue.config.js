8d2572d7d898a3b5f3f4a7cb2ac4d61e2e404f50
73desi <2633043826@qq.com>
2022-07-13 16:24:27 +0800

    0.1.1修复跨域问题

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,23 @@ const webpack = require('webpack');
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
-    //支持jquery
-    plugins: [
-        new webpack.ProvidePlugin({
-            $:"jquery",
-            jQuery:"jquery",
-            "windows.jQuery":"jquery"
-        })
-    ]
+
 },
 
 })
-
+module.exports = {
+    publicPath: "./", // 公共路径(必须有的)
+    outputDir: "dist", // 输出文件目录
+    assetsDir: "static", //静态资源文件名称
+    lintOnSave: false,
+    productionSourceMap: false, //去除打包后js的map文件
+    chainWebpack: config => {
+        config.plugin('provide').use(webpack.ProvidePlugin, [{
+            $: 'jquery',
+            jquery: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }])
+    }
+}
 
